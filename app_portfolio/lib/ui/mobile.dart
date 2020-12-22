1083c13de0b3b25420b56862cdfaffef216eeff6
Sagnik Bhattacharya <sagnikbhattachaya1506@gmail.com>
2020-12-22 20:29:20 +0530

    hllo

---
+++ app_portfolio/lib/ui/mobile.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Mobile extends StatefulWidget {
+  @override
+  _MobileState createState() => _MobileState();
+}
+
+class _MobileState extends State<Mobile> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+
+    );
+  }
+}
