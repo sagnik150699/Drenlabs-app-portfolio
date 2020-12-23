6a3470632c0d1ad60804594228ff3ad786df7aac
Sagnik Bhattacharya <sagnikbhattachaya1506@gmail.com>
2020-12-23 19:56:02 +0530

    ,...

--- app_portfolio/lib/ui/mobile.dart
+++ app_portfolio/lib/ui/mobile.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Mobile extends StatefulWidget {
   @override
@@ -12,12 +13,40 @@ class _MobileState extends State<Mobile> {
       home: Scaffold(
         body: Container(
           decoration: BoxDecoration(
-            gradient: LinearGradient(
-              begin: Alignment.topLeft,
-                end: Alignment.bottomRight,
-                colors: [Colors.red,Colors.purple,Colors.yellow])
+              gradient: LinearGradient(
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                  colors: [Colors.teal,Colors.purple,Colors.yellow])
           ),
+          child: SafeArea(
+            child: ListView(
+              padding: const EdgeInsets.all(8),
+              children: <Widget>[
+                Container(
+                  height: 100,
+                  color: Colors.amber[200].withOpacity(0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text("Portfolio project by",
+                        style: GoogleFonts.roboto(
+                          fontSize: 20,textStyle: TextStyle(
+                          color: Colors.white
+                        )
+                      ),),
+                      Text(" Nicola Cestaro", style: TextStyle(
+                        fontWeight: FontWeight.bold, color: Colors.white,
+                        fontSize: 20
+                      ),)
+                    ],
+                  ),
 
+
+                ),
+
+              ],
+            ),
+          ),
         ),
       ),
     );
---
+++ app_portfolio/lib/variables.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+
+class Variables{
+
+Text text( String str){
+
+}
+
+
+
+}
