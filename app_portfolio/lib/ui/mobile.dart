ab468dcd3d206d0ed1962dbbcfb8e4d3545aff9c
Sagnik Bhattacharya <sagnikbhattachaya1506@gmail.com>
2020-12-24 00:19:36 +0530

    Bluring Left

--- app_portfolio/lib/ui/mobile.dart
+++ app_portfolio/lib/ui/mobile.dart
@@ -1,54 +1,249 @@
+import 'dart:ui';
+
+import 'package:app_portfolio/variables.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:flutter_svg/svg.dart';
 
 class Mobile extends StatefulWidget {
   @override
   _MobileState createState() => _MobileState();
 }
 
+Variables variables = new Variables();
+
 class _MobileState extends State<Mobile> {
+  ScrollController controller = ScrollController();
+
+  Color colour1 = Color(0xFF77CBDC);
+  Color colour2 = Color(0xFF214C70);
+  final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
+
+  @override
+  void initState() {
+    super.initState();
+    controller.addListener(() {
+      if (controller.offset == 0) {
+        setState(() {
+          colour1 = Color(0xFF77CBDC);
+          colour2 = Color(0xFF214C70);
+        });
+      } else if (controller.offset > 20) {
+        setState(() {
+          colour1 = Color(0xFF77CBDC);
+          colour2 = Color(0xFF214C70);
+        });
+      }
+
+      print(controller.offset);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: Container(
-          decoration: BoxDecoration(
-              gradient: LinearGradient(
-                  begin: Alignment.topLeft,
-                  end: Alignment.bottomRight,
-                  colors: [Colors.teal,Colors.purple,Colors.yellow])
-          ),
-          child: SafeArea(
-            child: ListView(
-              padding: const EdgeInsets.all(8),
-              children: <Widget>[
-                Container(
-                  height: 100,
-                  color: Colors.amber[200].withOpacity(0),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text("Portfolio project by",
-                        style: GoogleFonts.roboto(
-                          fontSize: 20,textStyle: TextStyle(
-                          color: Colors.white
-                        )
-                      ),),
-                      Text(" Nicola Cestaro", style: TextStyle(
-                        fontWeight: FontWeight.bold, color: Colors.white,
-                        fontSize: 20
-                      ),)
-                    ],
-                  ),
-
-
+        body: Stack(
+          children: <Widget>[
+            Container(
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.bottomRight,
+                  end: Alignment.topLeft,
+                  colors: [colour1, colour2],
                 ),
-
+              ),
+            ),
+            Stack(
+              fit: StackFit.expand,
+              children: [
+                BackdropFilter(
+                  filter: ImageFilter.blur(sigmaX: 5.0, sigmaY: 5.0),
+                  child: Container(),
+                ),
+                Align(
+                  alignment: Alignment.topRight,
+                  child: SvgPicture.asset("images/Ovalbig.svg"),
+                ),
               ],
             ),
-          ),
+            Align(
+                alignment: Alignment.centerRight,
+                child: SvgPicture.asset("images/smallrectangle.svg")),
+            Align(
+                alignment: Alignment.bottomLeft,
+                child: SvgPicture.asset("images/Oval.svg")),
+            SafeArea(
+              child: ListView(
+//scrollDirection: Axis.horizontal,
+                controller: controller,
+                padding: const EdgeInsets.all(8),
+                children: <Widget>[
+                  Container(
+                    height: 100,
+                    color: Colors.amber[200].withOpacity(0),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        variables.texts(
+                            "Portfolio project by", 25.0, FontWeight.normal),
+                        variables.texts("Nicola Cestaro", 25.0, FontWeight.bold)
+                      ],
+                    ),
+                  ),
+                  SizedBox(
+                    height: 40,
+                  ),
+                  CarouselSlider(
+//carouselController: controller,
+                      options: CarouselOptions(
+                        height: 400,
+                        aspectRatio: 16 / 9,
+                        viewportFraction: 0.9,
+                        initialPage: 0,
+                        enableInfiniteScroll: true,
+                        reverse: false,
+                        autoPlay: true,
+                        autoPlayInterval: Duration(seconds: 3),
+                        autoPlayAnimationDuration: Duration(milliseconds: 800),
+                        autoPlayCurve: Curves.fastOutSlowIn,
+                        enlargeCenterPage: true,
+                        scrollDirection: Axis.horizontal,
+                      ),
+                      items: [
+                        Container(
+                          height: 250,
+                          child: Column(
+                            children: [
+                              variables.texts(
+                                  "Some App Name", 34.0, FontWeight.bold),
+                              variables.texts(
+                                  "Goes Here", 34.0, FontWeight.bold),
+                              SizedBox(height: 26),
+                              variables.texts(
+                                  "Revolutionize your work experience and expand your possibility thanks to the resources that the web has to offer ",
+                                  20.0,
+                                  FontWeight.w300),
+                              SizedBox(height: 50),
+                              variables.texts(
+                                  "MasterPlan", 26.0, FontWeight.w200),
+//SizedBox(height: 0),
+                            ],
+                          ),
+                        ),
+                      ]),
+                  CarouselSlider(
+                    options: CarouselOptions(
+                      height: 300,
+                      aspectRatio: 16 / 9,
+                      viewportFraction: 0.9,
+                      initialPage: 0,
+                      enableInfiniteScroll: true,
+                      reverse: false,
+                      autoPlay: true,
+                      autoPlayInterval: Duration(seconds: 3),
+                      autoPlayAnimationDuration: Duration(milliseconds: 800),
+                      autoPlayCurve: Curves.fastOutSlowIn,
+                      enlargeCenterPage: true,
+                      scrollDirection: Axis.horizontal,
+                    ),
+                    items: [
+                      Image(
+                        height: 200,
+                        image: AssetImage('images/phoneH.png'),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ),
+
+            // Image(image: AssetImage("images/Group.svg"))
+          ],
         ),
       ),
     );
   }
 }
+
+//
+// SafeArea(
+// child: ListView(
+// //scrollDirection: Axis.horizontal,
+// controller: controller,
+// padding: const EdgeInsets.all(8),
+// children: <Widget>[
+// Container(
+// height: 100,
+// color: Colors.amber[200].withOpacity(0),
+// child: Column(
+// mainAxisAlignment: MainAxisAlignment.center,
+// children: [
+// variables.texts("Portfolio project by", 25.0, FontWeight.normal),
+// variables.texts("Nicola Cestaro", 25.0, FontWeight.bold)
+// ],
+// ),
+// ),
+//
+// SizedBox(
+// height: 40,
+// ),
+//
+// CarouselSlider(
+// //carouselController: controller,
+// options: CarouselOptions(
+// height: 400,
+// aspectRatio: 16/9,
+// viewportFraction: 0.9,
+// initialPage: 0,
+// enableInfiniteScroll: true,
+// reverse: false,
+// autoPlay: true,
+// autoPlayInterval: Duration(seconds: 3),
+// autoPlayAnimationDuration: Duration(milliseconds: 800),
+// autoPlayCurve: Curves.fastOutSlowIn,
+// enlargeCenterPage: true,
+// scrollDirection: Axis.horizontal,
+// ),
+// items:[Container(
+// height: 250,
+// child: Column(
+// children: [
+// variables.texts("Some App Name", 34.0,FontWeight.bold),
+// variables.texts("Goes Here", 34.0,FontWeight.bold),
+// SizedBox(height: 26),
+// variables.texts("Revolutionize your work experience and expand your possibility thanks to the resources that the web has to offer ", 20.0,FontWeight.w300),
+// SizedBox(height: 50),
+// variables.texts("MasterPlan", 26.0, FontWeight.w200),
+// //SizedBox(height: 0),
+// ],
+// ),
+// ),]
+// ),
+// CarouselSlider(
+// options: CarouselOptions(
+// height: 300,
+// aspectRatio: 16/9,
+// viewportFraction: 0.9,
+// initialPage: 0,
+// enableInfiniteScroll: true,
+// reverse: false,
+// autoPlay: true,
+// autoPlayInterval: Duration(seconds: 3),
+// autoPlayAnimationDuration: Duration(milliseconds: 800),
+// autoPlayCurve: Curves.fastOutSlowIn,
+// enlargeCenterPage: true,
+// scrollDirection: Axis.horizontal,
+// ),
+// items:
+// [
+// Image(
+// height: 200,
+// image: AssetImage('images/phoneH.png'),
+// ),
+// ], )
+// ],
+// ),
+// ),
--- app_portfolio/lib/variables.dart
+++ app_portfolio/lib/variables.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
-
-class Variables{
-
-Text text( String str){
-
-}
-
-
-
+class Variables {
+  Text texts(String str, size, fontWeight ) {
+   return Text(
+      str,
+     textAlign: TextAlign.center,
+      style: GoogleFonts.poppins(
+        fontSize: size,
+        textStyle: TextStyle(color: Colors.white, fontWeight: fontWeight),
+      ),
+    );
+  }
 }
